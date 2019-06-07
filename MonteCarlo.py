9401cd1a983f59d8b808afe9e231c158b92b5e45
nakata <nakataatsuya@gmail.com>
2019-06-07 09:37:15 +0900

    CPUの仮実装

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -3,13 +3,14 @@ import random
 
 
 class Node:
-    def __init__(self, board, player, putableFunc, winFunc):
+    def __init__(self, board, player, putableFunc, winFunc,*args):
         self.board = board
         self.player = player
         self.putable = putableFunc
         self.win = winFunc
         self.wins = 0
         self.n = 0
+        self.args=args
     def get_Putable(self, board, player):
         ls = []
         for i in range(board.shape[0]):
@@ -20,12 +21,12 @@ class Node:
         return ls
     def get_values(self):
         return (self.n, self.wins, self.player)
+    def next_player(self):
+        return self.player%2+1
 
 
 class Root(Node):
     UCT = lambda win, n, N: win / n + 1.4 * np.sqrt(np.log(N) / n)
-    def add_child(self, ch):
-        self.chldren.append(ch)
     def getResult(self, num):
         while self.n < num:
             nd = self.selectNode()
@@ -33,7 +34,10 @@ class Root(Node):
         self.chldren[np.argmax([c.n for c in self.chldren])]
     def selectNode(self):
         return self.chldren[np.argmax([Node.UCT(win, n, self.N) for win, n, p in [c.get_value for c in self.chldren]])]
-
+    def create_children(self):
+        boards=self.get_Putable(self.board,self.player)
+        player=self.next_player()
+        self.children=[Child(board,player,self.putable,self.win,xy) for xy,board in boards]
 
 class Child(Node):
     def playout(self):
